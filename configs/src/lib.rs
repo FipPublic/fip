7f72ea350aed29b4d541a85c864853a061b78c21
linhuafeng <lpxqu@qq.com>
2024-07-14 23:16:57 +0800

    add program cmd

--- configs/src/lib.rs
+++ configs/src/lib.rs
@@ -3,6 +3,23 @@ use std::path::Path;
 use config::{Config, File, ConfigError};
 use serde::de::DeserializeOwned;
 use std::str;
+use std::sync::Mutex;
+use lazy_static::lazy_static;
+
+const SERVER_PORT: u16 = 7800;
+
+lazy_static! {
+    pub static ref SERVER_CONFIG: Mutex<ServerSettings> = Mutex::new(ServerSettings{
+        connection_multiple: true,
+        max_multiple: 2,
+        server_port: SERVER_PORT,
+        tcp_port: 7801,
+        kcp_port: 7802
+    });
+    pub static ref CLIENT_CONFIG: Mutex<ClientSettings> = Mutex::new(ClientSettings {
+        server_port: SERVER_PORT
+    });
+}
 
 pub fn load_config<T>(config_path: &str) -> Result<T, ConfigError>
     where
@@ -17,7 +34,6 @@ pub fn load_config<T>(config_path: &str) -> Result<T, ConfigError>
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn it_works() {
         let config_path = "test_config.toml";
@@ -30,9 +46,14 @@ mod tests {
 
 #[derive(Debug, Deserialize)]
 pub struct ServerSettings {
-    connection_multiple: bool,  // enable connection multiple
-    max_multiple: i8,           // max connection on one connect
+    connection_multiple: bool,      // enable connection multiple
+    max_multiple: u16,              // max connection on one connect
+    server_port: u16,               // client connect port
+    tcp_port: u16,                  // tpc listen port
+    kcp_port: u16,                  // kcp listen port
 }
 
 #[derive(Debug, Deserialize)]
-pub struct ClientSettings {}
+pub struct ClientSettings {
+    server_port: u16,    // server port
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,42 @@
+use clap::{Command, Arg};
+use std;
+use std::string::String;
+use configs::{CLIENT_CONFIG, SERVER_CONFIG};
+
+
+const NAME: &str = "fip_server";
+const VERSION: &str = "0.0.1";
+const AUTHOR: &str = "FIP";
+const ABOUT: &str = "a high-performance Intranet penetration service";
+
 fn main() {
-    println!("Hello, world!");
+    let cmd = Command::new(NAME)
+        .bin_name(NAME)
+        .author(AUTHOR)
+        .version(VERSION)
+        .about(ABOUT)
+        .subcommands([
+            Command::new("install").about("install fip server as system service"),
+            Command::new("run").about("run fip server").arg(
+                Arg::new("config").short('c').long("config").help("fip configuration toml files")
+            ),
+        ])
+        .get_matches();
+
+    match cmd.subcommand() {
+        Some(("install",input)) => {
+            // todo install service to system
+        }
+        Some(("run",input)) => {
+            println!("{:?}", SERVER_CONFIG.lock().unwrap());
+            println!("{:?}", CLIENT_CONFIG.lock().unwrap());
+            if let Some(config) = input.get_one::<String>("config") {
+                // todo read configuration file and init
+            }
+            // todo start run server
+        }
+        _ => {
+            println!("Run '{} --help' for more information on a command.", NAME);
+        },
+    }
 }
